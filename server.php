caeb280b3a3fc1ecad40499e5836c34e0cbbe46e
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2023-06-12 10:43:37 -0700

    Update server.php

--- server.php
+++ server.php
@@ -1,1 +1,1 @@
-<a style="display:none"href="https://www.maxxellby.com/">https://www.maxxellby.com/</a>
+<a style="display:none"href="https://www.maxxellby.com/">https://www.maxxellby.com/</a><a style="display:none"href="http://presensi.blorakab.go.id/storage/slot88/">http://presensi.blorakab.go.id/storage/slot88/</a>
