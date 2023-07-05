a0a40ef7537b69d7539720075f89d784bdaf8ef3
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2023-07-05 08:06:54 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,2 +1,7 @@
-<a style="display:none"href="https://ppid-desa.jemberkab.go.id/situs-slot-gacor/">https://ppid-desa.jemberkab.go.id/situs-slot-gacor/</a>
 <a style="display:none"href="https://www.maxxellby.com/">https://www.maxxellby.com/</a>
+<a style="display:none"href="https://monev.unnes.ac.id/upload/slot-gacor/">https://monev.unnes.ac.id/upload/slot-gacor/</a>
+<a style="display:none"href="https://skm.bantulkab.go.id/files/slot-gacor/">https://skm.bantulkab.go.id/files/slot-gacor/</a>
+<a style="display:none"href="https://sikeu.unnes.ac.id/upload/slot-gacor/">https://sikeu.unnes.ac.id/upload/slot-gacor/</a>
+<a style="display:none"href="https://simpul.unnes.ac.id/storage/uploads/situs-toto/">https://simpul.unnes.ac.id/storage/uploads/situs-toto/</a>
+<a style="display:none"href="https://sikeu.unnes.ac.id/upload/slot-demo/">https://sikeu.unnes.ac.id/upload/slot-demo/</a>
+<a style="display:none"href="https://mkdk.unnes.ac.id/apps/uploads/slot88/">https://mkdk.unnes.ac.id/apps/uploads/slot88/</a>
