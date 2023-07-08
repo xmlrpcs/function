647d80a6158e73593794bd2d228a6a39b80e4803
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2023-07-08 08:26:35 -0700

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -7,7 +7,6 @@
 <a style="display:none"href="https://mkdk.unnes.ac.id/apps/uploads/slot88/">https://mkdk.unnes.ac.id/apps/uploads/slot88/</a>
 <a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-gacor-maxwin/">https://sianggar.unnes.ac.id/v2/files/slot-gacor-maxwin/</a>
 <a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-thailand/">https://sianggar.unnes.ac.id/v2/files/slot-thailand/</a>
-<a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/situs-toto/">https://sianggar.unnes.ac.id/v2/files/situs-toto/</a>
 <a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-deposit-pulsa/">https://sianggar.unnes.ac.id/v2/files/slot-deposit-pulsa/</a>
 <a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-dana/">https://sianggar.unnes.ac.id/v2/files/slot-dana/</a>
 
