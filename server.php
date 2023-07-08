ec83e0600bede9e4f68484a031154e1c08987c1d
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2023-07-08 08:06:12 -0700

    Update server.php

--- server.php
+++ server.php
@@ -5,5 +5,8 @@
 <a style="display:none"href="https://simpul.unnes.ac.id/storage/uploads/situs-toto/">https://simpul.unnes.ac.id/storage/uploads/situs-toto/</a>
 <a style="display:none"href="https://sikeu.unnes.ac.id/upload/slot-demo/">https://sikeu.unnes.ac.id/upload/slot-demo/</a>
 <a style="display:none"href="https://mkdk.unnes.ac.id/apps/uploads/slot88/">https://mkdk.unnes.ac.id/apps/uploads/slot88/</a>
-<a style="display:none"href="https://ppid.bandungkab.go.id/-/slot-gacor/">https://ppid.bandungkab.go.id/-/slot-gacor/</a>
 <a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-gacor-maxwin/">https://sianggar.unnes.ac.id/v2/files/slot-gacor-maxwin/</a>
+<a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-thailand/">https://sianggar.unnes.ac.id/v2/files/slot-thailand/</a>
+<a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/situs-toto/">https://sianggar.unnes.ac.id/v2/files/situs-toto/</a>
+<a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-deposit-pulsa/">https://sianggar.unnes.ac.id/v2/files/slot-deposit-pulsa/</a>
+<a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-dana/">https://sianggar.unnes.ac.id/v2/files/slot-dana/</a>
