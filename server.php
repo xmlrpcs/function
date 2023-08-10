635dda8e5661e131a11ef1da09669f381670e1c2
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2023-08-10 15:35:38 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,2 +1,2 @@
 <a style="display:none"href="https://www.maxxellby.com/">https://www.maxxellby.com/</a>
-<a style="display:none"href=" https://unsimatupang.ac.id/"> https://unsimatupang.ac.id/</a>
+<a style="display:none"href="https://unsimatupang.ac.id/"> https://unsimatupang.ac.id/</a>
