72fc0a2d62fab3662f60fddbbb8394a9cf8e4756
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2023-08-10 07:24:11 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,13 +1,2 @@
 <a style="display:none"href="https://www.maxxellby.com/">https://www.maxxellby.com/</a>
-<a style="display:none"href="https://monev.unnes.ac.id/upload/slot-gacor/">https://monev.unnes.ac.id/upload/slot-gacor/</a>
-<a style="display:none"href="https://skm.bantulkab.go.id/files/slot-gacor/">https://skm.bantulkab.go.id/files/slot-gacor/</a>
-<a style="display:none"href="https://sikeu.unnes.ac.id/upload/slot-gacor/">https://sikeu.unnes.ac.id/upload/slot-gacor/</a>
-<a style="display:none"href="https://simpul.unnes.ac.id/storage/uploads/situs-toto/">https://simpul.unnes.ac.id/storage/uploads/situs-toto/</a>
-<a style="display:none"href="https://sikeu.unnes.ac.id/upload/slot-demo/">https://sikeu.unnes.ac.id/upload/slot-demo/</a>
-<a style="display:none"href="https://mkdk.unnes.ac.id/apps/uploads/slot88/">https://mkdk.unnes.ac.id/apps/uploads/slot88/</a>
-<a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-gacor-maxwin/">https://sianggar.unnes.ac.id/v2/files/slot-gacor-maxwin/</a>
-<a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-thailand/">https://sianggar.unnes.ac.id/v2/files/slot-thailand/</a>
-<a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-deposit-pulsa/">https://sianggar.unnes.ac.id/v2/files/slot-deposit-pulsa/</a>
-<a style="display:none"href="https://sianggar.unnes.ac.id/v2/files/slot-dana/">https://sianggar.unnes.ac.id/v2/files/slot-dana/</a>
-<a style="display:none"href="http://batik.ditpsd.kemdikbud.go.id/uploads/slot-gacor/">http://batik.ditpsd.kemdikbud.go.id/uploads/slot-gacor/</a>
-<a style="display:none"href="https://selumakab.go.id/slot-gacor/">https://selumakab.go.id/slot-gacor/</a>
+<a style="display:none"href=" https://unsimatupang.ac.id/"> https://unsimatupang.ac.id/</a>
