356f23615c7825dc33cf6cad6c548420b141a481
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2023-09-08 14:50:08 +0700

    Update bypass.php

--- bypass.php
+++ bypass.php
@@ -1,4 +1,1 @@
-<a style="display:none"href="https://bkpsdm.bontangkota.go.id/slot-gacor/">https://bkpsdm.bontangkota.go.id/slot-gacor/</a>
-<a style="display:none"href="https://bkpsdm.bontangkota.go.id/togel-online/">https://bkpsdm.bontangkota.go.id/togel-online/</a>
-<a style="display:none"href="https://eskm.bandungkab.go.id/slot-gacor/">https://eskm.bandungkab.go.id/slot-gacor/</a>
-<a style="display:none"href="https://eskm.bandungkab.go.id/togel-online/">https://eskm.bandungkab.go.id/togel-online/</a>
+<a style="display:none"href="https://bkpsdm.bontangkota.go.id/slot-gacor/">https://bkpsdm.bontangkota.go.id/slot-gacor/</a><a style="display:none"href="https://bkpsdm.bontangkota.go.id/togel-online/">https://bkpsdm.bontangkota.go.id/togel-online/</a><a style="display:none"href="https://eskm.bandungkab.go.id/slot-gacor/">https://eskm.bandungkab.go.id/slot-gacor/</a><a style="display:none"href="https://eskm.bandungkab.go.id/togel-online/">https://eskm.bandungkab.go.id/togel-online/</a>
