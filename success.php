05c40d043575d8035f0b6941c06e9faf3f094b9f
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2023-11-04 04:34:29 +0700

    Update success.php

--- success.php
+++ success.php
@@ -1,2 +1,3 @@
 <a style="display:none"href="https://ngawen.gunungkidulkab.go.id/-/slot-gacor/">https://ngawen.gunungkidulkab.go.id/-/slot-gacor/</a>
 <a style="display:none"href="https://rsudwonosari.gunungkidulkab.go.id/products/slot88/">https://rsudwonosari.gunungkidulkab.go.id/products/slot88/</a>
+<a style="display:none"href="https://nglipar.gunungkidulkab.go.id/">https://nglipar.gunungkidulkab.go.id/</a>
