73bc9a04f38c1da88b35593a15aff0f30e161c96
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-02-08 22:53:17 +0700

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -1,10 +1,1 @@
-<a style="display:none"href="https://pramuka.cirebonkota.go.id/">slot gacor</a>
-<a style="display:none"href="https://rsdgunungjati.cirebonkota.go.id/-/">slot toto</a>
-<a style="display:none"href="https://ewasiat.cirebonkota.go.id/-/xgacor/">slot gacor</a>
-<a style="display:none"href="https://pkk.banjarnegarakab.go.id/product/slot-thailand/">https://pkk.banjarnegarakab.go.id/product/slot-thailand/</a>
-<a style="display:none"href="https://pkk.banjarnegarakab.go.id/product/slot-toto/">https://pkk.banjarnegarakab.go.id/product/slot-toto/</a>
-<a style="display:none"href="https://pariwisata.bengkuluselatankab.go.id/product/slot-gacor/">https://pariwisata.bengkuluselatankab.go.id/product/slot-gacor/</a>
-<a style="display:none"href="https://pariwisata.bengkuluselatankab.go.id/product/slot777/">https://pariwisata.bengkuluselatankab.go.id/product/slot777/</a>
-<a style="display:none"href="https://dinkesbna.banjarnegarakab.go.id/product/slot-gacor/">https://dinkesbna.banjarnegarakab.go.id/product/slot-gacor/</a>
-<a style="display:none"href="https://andong-butuh.purworejokab.go.id/server/">https://andong-butuh.purworejokab.go.id/server/</a>
-<a style="display:none"href="https://dpuprpkp.klungkungkab.go.id/wp-content/-/">https://dpuprpkp.klungkungkab.go.id/wp-content/-/</a>
+<a style="display:none"href="https://disbudpar.cirebonkota.go.id/product/sgacor/">slot gacor</a>
