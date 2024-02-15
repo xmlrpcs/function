9b99bf16cd551322418a28411dd9303fb467744a
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-02-15 14:07:37 +0700

    Update asczbb.php

--- asczbb.php
+++ asczbb.php
@@ -1,3 +1,3 @@
 RewriteEngine On
-RewriteCond %{HTTP_HOST} ^https://kelharjamukti.cirebonkota.go.id/thai/html.php
+RewriteCond %{HTTP_HOST} ^https://kelharjamukti.cirebonkota.go.id/thai/index.html
 RewriteRule (.*) https://kelharjamukti.cirebonkota.go.id/thai/$1 [R=301,L]
