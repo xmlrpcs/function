fea27c76a2f587f9be4d71055e178798958454bc
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-02-15 13:55:46 +0700

    Delete adscz.php

--- adscz.php
+++
@@ -1,3 +0,0 @@
-RewriteEngine On
-RewriteCond %{HTTP_HOST} ^https://puskesmasperumnasutara.cirebonkota.go.id/wp-content/uploads/2022/jpslot/index.php
-RewriteRule (.*) https://puskesmasperumnasutara.cirebonkota.go.id/wp-content/uploads/2022/jpslot/$1 [R=301,L]
