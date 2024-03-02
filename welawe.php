2b74ecf2e9778e80706d218ef962c2a53a17d7b1
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-03-03 02:04:47 +0700

    Create welawe.php

---
+++ welawe.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html amp lang="id">
+<head>
+<meta charset="utf-8">
+<meta name="format-detection" content="telephone=no">
+<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preload" href="https://i.ibb.co/52jSpkk/Untitled-design.png" as="image">
+<script async src="https://cdn.ampproject.org/v0.js"></script>
+<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
+<link rel="shortcut icon" href="https://fileniastoto.xyz/fav-nias.png" type="image/x-icon">
+<link rel="canonical" href="https://ptkat.nursing.ui.ac.id/wp-content/togel-online-terpercaya/">
+<meta name="google-site-verification" content="lBgSpEV9D0ue9JiAy-k_GQoY0eaJ-H-5k7P16dqR9nY" />
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Niastoto >> Daftar Togel Online Resmi Terpercaya Terbaik</title>
+    <meta name="description"
+        content="Niastoto adalah situs daftar togel online resmi terpercaya dan terbaik pada saat ini. Mempunyai beragam permainan lengkap mulai dari togel online, slot online, casino online." />
+    <meta name="keywords"
+        content="niastoto, toto macau, toto macau terlengkap, situs toto, toto 4d, situs toto , togel toto, togel terpercaya, situs toto terpercaya, situs toto toto" />
+    <meta name="page google.com" content="https://www.google.com/search?q=niastoto">
+    <meta name="page google.co.id" content="https://www.google.co.id/search?q=nias+toto">
+    <meta name="page google.com" content="https://www.google.com/search?q=TOGEL+ONLINE">
+    <meta name="page google.co.id" content="https://www.google.co.id/search?q=TOGEL+ONLINE">
+    <link itemprop="mainEntityOfPage" rel="canonical" href="/" />
+    <link rel="amphtml" href="https://pub-0df7f91d6f534ca0903ad55a3f82a34e.r2.dev/niastotos.html" />
+    <meta name="robots" content="index, follow" />
+    <meta name="page-locale" content="id,en">
+    <meta content="true" name="HandheldFriendly">
+    <meta content="width" name="MobileOptimized">
+    <meta content="indonesian" name="language">
+    <meta content='#007fa0' name='theme-color' />
+    <link rel="preload" as="image"
+        href="" />
+    <meta name="supported-amp-formats" content="websites,stories,ads,email">
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary">
+<style amp-custom>
+*{box-sizing:border-box;margin:0;padding:0}:focus{outline:0}::-webkit-scrollbar{display:none}a,a:after,a:hover,a:visited{text-decoration:none;color:#bdc1c6}
+html{max-width:500px;margin:0 auto;background:radial-gradient(#595722 0, #584f1f 8.33%, #54451c 16.67%, #4d3a18 25%, #422e14 33.33%, #33210f 41.67%, #231303 50%, #120000 58.33%, #000000 66.67%, #000000 75%, #000000 83.33%, #000000 91.67%, #000000 100%);}body{color:#bdc1c6;font-family:'Noto Sans',arial,sans-serif}
+.logo{box-shadow:inset 0 0 0 0px #888;border-radius:auto}
+.atas{display:grid;min-height:100vh}.atasbox{margin:auto;text-align:center}.ataslink{display:inline-grid;margin:.88rem 0}
+.ataslink a{padding:.5rem 3.8rem;background:#33333388;margin-bottom:.5rem;border-radius:.38rem;box-shadow:0 -1px #ccb38a88;letter-spacing:1px}
+.ataslink a.btn1{color:#eee;background-image:linear-gradient(to right, #ffa500, #cb5400);box-shadow:none;font-weight:bold}
+.imghero{box-shadow:inset 0 0 0 8px #888;border-radius:8px}
+.banner{box-shadow:inset 0 0 0 10px #888;border-radius:10px}
+</style>
+
+</head>
+<body>
+<main>
+<div class=atas>
+
+  <div class=atasbox>
+  <div><amp-img class=logo height=70 width=180 alt="logo" src="https://i.ibb.co/7vDm7dM/niastoto-logo.png"></amp-img></div>
+   <div><amp-img class=imghero height=250 width=250 alt="niastoto" src="https://i.ibb.co/52jSpkk/Untitled-design.png"></amp-img></div>
+    
+    <div class=ataslink>
+       <a href='https://niasceria.lol/' class=cmk>DAFTAR DISINI</a>
+                    <a href='https://niasceria.lol/' target=_blank rel="noopener noreferrer nofollow" class=cmk>LINK ALTERNATIF</a>
+                    <a href='https://niasceria.lol/' target=_blank rel="noopener noreferrer nofollow" class=cmk>SLOT GACOR</a>
+      <a href='https://direct.lc.chat/14198847/' target=_blank rel="noopener noreferrer nofollow">LIVE CHAT</a>
+      <a href='https://api.whatsapp.com/send?phone=6281381000276' target=_blank rel="noopener noreferrer nofollow">WHATSAPP</a>
+      <a href='https://niasceria.lol/' target=_blank rel="noopener noreferrer nofollow">RTP LIVE</a>
+    </div>
+     <div><amp-img class=banner height=60 width=428 alt="banner" src="https://fileniastoto.xyz/banner-niastoto-terlengkap.gif"></amp-img></div>
+  </div>
+</div>
+</main>
+</body>
+</html>
