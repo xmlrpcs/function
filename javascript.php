025b3b38c60750678185ac9ded814d9c49c1be28
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-03-08 07:03:17 +0700

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -1,2 +1,1 @@
-<a style="display:none"href="https://inlislite-muktiwari.bekasikab.go.id/thailand/">slot thailand</a>
-<a style="display:none"href="http://ptkat.nursing.ui.ac.id/error/">slot thailand</a>
+<a style="display:none"href="https://p3ekalimantan.menlhk.go.id/wp-content/uploads/">https://p3ekalimantan.menlhk.go.id/wp-content/uploads/</a>
