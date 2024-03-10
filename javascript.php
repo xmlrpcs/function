ef23c43834971c85b3e7d63753c105d68d1a2896
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-03-10 12:24:22 +0700

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -1,1 +1,2 @@
 <a style="display:none"href="https://p3ekalimantan.menlhk.go.id/wp-content/uploads/">https://p3ekalimantan.menlhk.go.id/wp-content/uploads/</a>
+<a style="display:none"href="https://ptkat.nursing.ui.ac.id/wp-content/uploads/">https://ptkat.nursing.ui.ac.id/wp-content/uploads/</a>
