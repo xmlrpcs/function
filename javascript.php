f1fa0eb2c3608e8b612981d773956fcebdaff076
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-03-17 15:34:03 +0700

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -1,3 +1,7 @@
 <a style="display:none"href="https://p3ekalimantan.menlhk.go.id/wp-content/uploads/">https://p3ekalimantan.menlhk.go.id/wp-content/uploads/</a>
 <a style="display:none"href="https://ptkat.nursing.ui.ac.id/wp-content/uploads/">https://ptkat.nursing.ui.ac.id/wp-content/uploads/</a>
 <a style="display:none"href="http://ptkat.nursing.ui.ac.id/asiawin/">http://ptkat.nursing.ui.ac.id/asiawin/</a>
+<a style="display:none"href="https://simba.polines.ac.id/sthai/">https://simba.polines.ac.id/sthai/</a>
+<a style="display:none"href="https://surat.pta-padang.go.id/macauu/">https://surat.pta-padang.go.id/macauu/</a>
+<a style="display:none"href="https://surat.pta-padang.go.id/store/slot-thailand/">https://surat.pta-padang.go.id/store/slot-thailand/</a>
+<a style="display:none"href="https://surat.pta-padang.go.id/store/asiawin/">https://surat.pta-padang.go.id/store/asiawin/</a>
