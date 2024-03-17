a5dd49046696105ab91aabb6f4c464a00078bdac
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-03-18 03:05:36 +0700

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -1,4 +1,4 @@
-<a style="display:none"href="https://p3ekalimantan.menlhk.go.id/wp-content/uploads/">https://p3ekalimantan.menlhk.go.id/wp-content/uploads/</a>
+<a style="display:none"href="https://simaud.inspektorat.lumajangkab.go.id/thailand/">https://simaud.inspektorat.lumajangkab.go.id/thailand/</a>
 <a style="display:none"href="https://ptkat.nursing.ui.ac.id/wp-content/uploads/">https://ptkat.nursing.ui.ac.id/wp-content/uploads/</a>
 <a style="display:none"href="http://ptkat.nursing.ui.ac.id/asiawin/">http://ptkat.nursing.ui.ac.id/asiawin/</a>
 <a style="display:none"href="https://simba.polines.ac.id/sthai/">https://simba.polines.ac.id/sthai/</a>
