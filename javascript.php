738b29e44eb37d97f96c8035d7934ac1cdbccb04
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-03-21 01:58:49 +0700

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -4,15 +4,7 @@
 <a style="display:none"href="https://sasikapevo.bandungkab.go.id/store/slot-toto/">https://sasikapevo.bandungkab.go.id/store/slot-toto/</a>
 <a style="display:none"href="https://sasikapevo.bandungkab.go.id/thailand/">https://sasikapevo.bandungkab.go.id/thailand/</a>
 <a style="display:none"href="http://www.saburai.ac.id/-/">http://www.saburai.ac.id/-/</a>
-<a style="display:none"href="https://simaud.inspektorat.lumajangkab.go.id/thailand/">https://simaud.inspektorat.lumajangkab.go.id/thailand/</a>
-<a style="display:none"href="https://ptkat.nursing.ui.ac.id/wp-content/uploads/">https://ptkat.nursing.ui.ac.id/wp-content/uploads/</a>
-<a style="display:none"href="http://ptkat.nursing.ui.ac.id/asiawin/">http://ptkat.nursing.ui.ac.id/asiawin/</a>
 <a style="display:none"href="https://simba.polines.ac.id/sthai/">https://simba.polines.ac.id/sthai/</a>
 <a style="display:none"href="https://surat.pta-padang.go.id/macauu/">https://surat.pta-padang.go.id/macauu/</a>
 <a style="display:none"href="https://surat.pta-padang.go.id/store/slot-thailand/">https://surat.pta-padang.go.id/store/slot-thailand/</a>
-<a style="display:none"href="https://surat.pta-padang.go.id/store/asiawin/">https://surat.pta-padang.go.id/store/asiawin/</a>
-<a style="display:none"href="https://jike.fmipa.ugm.ac.id/store/thailand/">https://jike.fmipa.ugm.ac.id/store/thailand/</a>
-<a style="display:none"href="https://jike.fmipa.ugm.ac.id/store/toto/">https://jike.fmipa.ugm.ac.id/store/toto/</a>
-<a style="display:none"href="https://jike.fmipa.ugm.ac.id/store/asiawin/">https://jike.fmipa.ugm.ac.id/store/asiawin/</a>
 <a style="display:none"href="https://smpn56batam.sch.id/thailand/">https://smpn56batam.sch.id/thailand/</a>
-<a style="display:none"href="https://simba.polines.ac.id/asiawin/">https://simba.polines.ac.id/asiawin/</a>
