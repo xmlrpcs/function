b8fb761b25682a7900ac9b1dcd585c78280316c2
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-08-29 12:18:32 +0700

    Update tsc.php

--- tsc.php
+++ tsc.php
@@ -2,19 +2,4 @@
 <p><a style="display:none" href="https://mcherifbassiouni.com/">slot gacor</a></p>
 <p><a style="display:none" href="https://www.jesushuertadesoto.com/">slot gacor</a></p>
 <p><a style="display:none" href="https://www.mstringerelectrics.co.uk/">mstringerelectrics</a></p>
-<p><a style="display:none" href="https://pafiasahankab.org/">pafiasahankab.org</a></p>
-<p><a style="display:none" href="https://pafibatubarakab.org/">pafibatubarakab.org</a></p>
-<p><a style="display:none" href="https://pafidairikab.org/">pafidairikab.org</a></p>
-<p><a style="display:none" href="https://pafideliserdangkab.org/">pafideliserdangkab.org</a></p>
-<p><a style="display:none" href="https://pafikarokab.org/">pafikarokab.org</a></p>
-<p><a style="display:none" href="https://pafikotabinjai.org/">pafikotabinjai.org</a></p>
-<p><a style="display:none" href="https://pafikotapadangsidimpuan.org/">pafikotapadangsidimpuan.org</a></p>
-<p><a style="display:none" href="https://pafikotapematangsiantar.org/">pafikotapematangsiantar.org</a></p>
-<p><a style="display:none" href="https://pafikotasibolga.org/">pafikotasibolga.org</a></p>
-<p><a style="display:none" href="https://pafikotatanjungbalai.org/">pafikotatanjungbalai.org</a></p>
-<p><a style="display:none" href="https://pafilangkatkab.org/">pafilangkatkab.org</a></p>
-<p><a style="display:none" href="https://pafisamosirkab.org/">pafisamosirkab.org</a></p>
-<p><a style="display:none" href="https://pafisimalungunkab.org/">pafisimalungunkab.org</a></p>
-<p><a style="display:none" href="https://pafitobakab.org/">pafitobakab.org</a></p>
-<p><a style="display:none" href="https://pafibulucina.org/">pafibulucina.org</a></p>
 <p><a style="display:none" href="https://www.metrocorporationpune.com/">slot</a></p>
