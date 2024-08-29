090858b06d3c03a6f2a935eb3e8f6dda8ac6444f
xmlrpcs <127653439+xmlrpcs@users.noreply.github.com>
2024-08-29 12:39:44 +0700

    Update tsc.php

--- tsc.php
+++ tsc.php
@@ -1,5 +1,3 @@
 <p><a style="display:none" href="https://healthyweightregistry.org/">healthyweightregistry.org</a></p>
 <p><a style="display:none" href="https://mcherifbassiouni.com/">slot gacor</a></p>
 <p><a style="display:none" href="https://www.jesushuertadesoto.com/">slot gacor</a></p>
-<p><a style="display:none" href="https://www.mstringerelectrics.co.uk/">mstringerelectrics</a></p>
-<p><a style="display:none" href="https://www.metrocorporationpune.com/">slot</a></p>
